ad3a3c44fc94312636aeb8b3e996b60e780a5270
Renato Picco <56326785+repicco@users.noreply.github.com>
2020-03-17 19:59:27 -0300

    Sob.v2

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//Contagem Regressiva------------------------------------
+
+var target_date = new Date("march 27, 2020").getTime();
+var dias, horas, minutos, segundos;
+var regressiva = document.getElementById("regressiva");
+
+setInterval(function () {
+
+    var current_date = new Date().getTime();
+    var segundos_f = (target_date - current_date) / 1000;
+
+dias = parseInt(segundos_f / 86400);
+    segundos_f = segundos_f % 86400;
+    
+    horas = parseInt(segundos_f / 3600);
+    segundos_f = segundos_f % 3600;
+    
+    minutos = parseInt(segundos_f / 60);
+    segundos = parseInt(segundos_f % 60);
+
+    document.getElementById('dia').innerHTML = dias;
+document.getElementById('hora').innerHTML = horas;
+document.getElementById('minuto').innerHTML = minutos;
+document.getElementById('segundo').innerHTML = segundos;
+
+}, 1000);
+
+//------------------------------------------------------
+
+//Alerta de Saida---------------------------------------
+
+window.onbeforeunload = function(){alert ("Deseja mesmo sair do site?")};
+
+//------------------------------------------------------
